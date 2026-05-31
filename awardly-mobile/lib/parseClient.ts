5d2b67d09ca23174da5eef588841b5f211e0037b
joao0cb <joao.00000853774@unicap.br>
2026-05-31 12:34:46 -0300

    voltando parse

--- awardly-mobile/lib/parseClient.ts
+++ awardly-mobile/lib/parseClient.ts
@@ -4,11 +4,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 Parse.setAsyncStorage(AsyncStorage);
 
 Parse.initialize(
-  process.env.EXPO_PUBLIC_PARSE_APP_ID ?? '',
-  process.env.EXPO_PUBLIC_PARSE_JS_KEY ?? ''
+  process.env.EXPO_PUBLIC_PARSE_APP_ID!,
+  process.env.EXPO_PUBLIC_PARSE_JS_KEY!
 );
 
 Parse.serverURL =
-  process.env.EXPO_PUBLIC_PARSE_SERVER_URL ?? 'https://parseapi.back4app.com';
+  process.env.EXPO_PUBLIC_PARSE_SERVER_URL || 'https://parseapi.back4app.com';
 
 export default Parse;
+
+// Dependências necessárias:
+// npx expo install parse @react-native-async-storage/async-storage
