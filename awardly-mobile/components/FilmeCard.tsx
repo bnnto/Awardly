09827ca6706a19903c16e6f39496580f5dd53466
joao0cb <joao.00000853774@unicap.br>
2026-05-31 15:21:48 -0300

    removendo logs

--- awardly-mobile/components/FilmeCard.tsx
+++ awardly-mobile/components/FilmeCard.tsx
@@ -39,7 +39,6 @@ export default function FilmeCard({
   itemForcado,
   venceu = false,
 }: FilmeCardProps) {
-  console.log('CARD:', filme);
   const router = useRouter();
 
   const nomeItem = Array.isArray(itemForcado)
